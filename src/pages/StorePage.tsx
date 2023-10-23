588bf6d1543039ce38f396dbd3314af67f6f9920
Rômulo Henri <romulohenri000@gmail.com>
2023-10-23 09:50:10 -0300

    fix: Responsivity error

--- src/pages/StorePage.tsx
+++ src/pages/StorePage.tsx
@@ -246,6 +246,196 @@ const StorePage = () => {
         </div>
       </div>
 
+      <div className="productCard">
+        <div className="image-box">
+          <img src={genericProduct} />
+        </div>
+        <div className="content-box">
+          <h2>Nome do Produto</h2>
+          <p className='desc'>Descrição</p>
+          <div className="review-box">
+            <p>
+              <img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/>
+            </p>
+          </div>
+          <div className="price-box">
+            <span>R$ 24,00</span>
+            <p className="price">R$ 20,00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="productCard">
+        <div className="image-box">
+          <img src={genericProduct} />
+        </div>
+        <div className="content-box">
+          <h2>Nome do Produto</h2>
+          <p className='desc'>Descrição</p>
+          <div className="review-box">
+            <p>
+              <img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/>
+            </p>
+          </div>
+          <div className="price-box">
+            <span>R$ 24,00</span>
+            <p className="price">R$ 20,00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="productCard">
+        <div className="image-box">
+          <img src={genericProduct} />
+        </div>
+        <div className="content-box">
+          <h2>Nome do Produto</h2>
+          <p className='desc'>Descrição</p>
+          <div className="review-box">
+            <p>
+              <img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/>
+            </p>
+          </div>
+          <div className="price-box">
+            <span>R$ 24,00</span>
+            <p className="price">R$ 20,00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="productCard">
+        <div className="image-box">
+          <img src={genericProduct} />
+        </div>
+        <div className="content-box">
+          <h2>Nome do Produto</h2>
+          <p className='desc'>Descrição</p>
+          <div className="review-box">
+            <p>
+              <img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/>
+            </p>
+          </div>
+          <div className="price-box">
+            <span>R$ 24,00</span>
+            <p className="price">R$ 20,00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="productCard">
+        <div className="image-box">
+          <img src={genericProduct} />
+        </div>
+        <div className="content-box">
+          <h2>Nome do Produto</h2>
+          <p className='desc'>Descrição</p>
+          <div className="review-box">
+            <p>
+              <img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/>
+            </p>
+          </div>
+          <div className="price-box">
+            <span>R$ 24,00</span>
+            <p className="price">R$ 20,00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="productCard">
+        <div className="image-box">
+          <img src={genericProduct} />
+        </div>
+        <div className="content-box">
+          <h2>Nome do Produto</h2>
+          <p className='desc'>Descrição</p>
+          <div className="review-box">
+            <p>
+              <img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/>
+            </p>
+          </div>
+          <div className="price-box">
+            <span>R$ 24,00</span>
+            <p className="price">R$ 20,00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="productCard">
+        <div className="image-box">
+          <img src={genericProduct} />
+        </div>
+        <div className="content-box">
+          <h2>Nome do Produto</h2>
+          <p className='desc'>Descrição</p>
+          <div className="review-box">
+            <p>
+              <img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/>
+            </p>
+          </div>
+          <div className="price-box">
+            <span>R$ 24,00</span>
+            <p className="price">R$ 20,00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="productCard">
+        <div className="image-box">
+          <img src={genericProduct} />
+        </div>
+        <div className="content-box">
+          <h2>Nome do Produto</h2>
+          <p className='desc'>Descrição</p>
+          <div className="review-box">
+            <p>
+              <img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/>
+            </p>
+          </div>
+          <div className="price-box">
+            <span>R$ 24,00</span>
+            <p className="price">R$ 20,00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="productCard">
+        <div className="image-box">
+          <img src={genericProduct} />
+        </div>
+        <div className="content-box">
+          <h2>Nome do Produto</h2>
+          <p className='desc'>Descrição</p>
+          <div className="review-box">
+            <p>
+              <img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/>
+            </p>
+          </div>
+          <div className="price-box">
+            <span>R$ 24,00</span>
+            <p className="price">R$ 20,00</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="productCard">
+        <div className="image-box">
+          <img src={genericProduct} />
+        </div>
+        <div className="content-box">
+          <h2>Nome do Produto</h2>
+          <p className='desc'>Descrição</p>
+          <div className="review-box">
+            <p>
+              <img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/><img src={icon_star}/>
+            </p>
+          </div>
+          <div className="price-box">
+            <span>R$ 24,00</span>
+            <p className="price">R$ 20,00</p>
+          </div>
+        </div>
+      </div>
+
     </section>
   </>)
 }
