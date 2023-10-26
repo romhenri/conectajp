148db09a45df6fcf0839a10d38d72f474bc84c40
Rômulo Henri <romulohenri000@gmail.com>
2023-10-26 08:39:57 -0300

    improve: Google Maps

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -5,20 +5,62 @@ const MapPage = () => {
   let map: google.maps.Map;
 
   async function initMap(): Promise<void> {
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  map = new Map(document.getElementById("map") as HTMLElement, {
-    center: { lat: -7.1629418568226155, lng: -34.825555030682175 },
-    // -7.1629418568226155, -34.825555030682175
-    zoom: 12,
-  });
+    //@ts-ignore
+    const position = { lat: -7.1629958568226155, lng: -34.825555030682175 };
+
+    //@ts-ignore
+    const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary; //@ts-ignore
+    const { AdvancedMarkerView } = await google.maps.importLibrary("marker")  as google.maps.MarkerLibrary;
+
+    map = new Map(document.getElementById("map") as HTMLElement,
+    {
+      center: position,
+      zoom: 14,
+      streetViewControl: false,
+      // icon: iconBase + 'novo_marcador.png',
+    });
+
+    //@ts-ignore
+    new google.maps.Marker({
+      map: map,
+      position: position,
+      title: 'ECIT-JP'
+    });
+
+    //@ts-ignore
+    new google.maps.Marker({
+      map: map,
+      position: { lat: -7.1929958568226155, lng: -34.825555030682175 },
+      title: 'ECIT-JP'
+    });
+
+     //@ts-ignore
+     new google.maps.Marker({
+      map: map,
+      position: { lat: -7.1929958568226155, lng: -34.895555030682175 },
+      title: 'ECIT-JP'
+    });
+
+     //@ts-ignore
+      new google.maps.Marker({
+      map: map,
+      position: { lat: -7.1319958568226155, lng: -34.885555030682175 },
+      title: 'ECIT-JP'
+    });
   }
 
   initMap();
 
 
   return (
-    <div id="map"></div>
+    <>
+      <header className='page-header account-page'>
+        <h1>Mapa</h1>
+      </header>
+      <section className="map-section">
+        <div id="map"></div>
+      </section>
+    </>
   )
 }
 
