f08483cbc191990002f1417b3c54e9e7cd28c09a
Rômulo Henri <romulohenri000@gmail.com>
2023-11-15 21:00:52 -0300

    improve: HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import '../css/pages.css'
 import '../css/HomePages.css'
 
@@ -11,13 +12,48 @@ const HomePage: React.FC<HomePageProps> =
       data
     }
   ) => {
+
+  const navigate = useNavigate()
     
+  const toSports = () => {navigate(
+    'loja?q=dami%C3%A3o%20bike'
+    )}
+  const toBeauty = () => {navigate(
+    'loja?q=cleide%20cosm%C3%A9ticos'
+    )}
+  const toAnimals = () => {navigate(
+    'loja?q=pet%20est%C3%A9tica'
+    )} 
+  const toHealth = () => {navigate(
+    'loja?q=naturalles%20%26%20sa%C3%BAde'
+    )}
+   
   return (
     <main className='home-page'>
       <header className='page-header'>
         <h1>ConectaJP</h1>
       </header>
       <section className="home-section">
+         <h2 className='main'>Seja bem-vindo(a)!</h2>
+        <hr />
+        <h2>Categorias</h2>
+        <div className="grid">
+          <div onClick={toSports}>
+            Esportes
+          </div>
+          <div onClick={toBeauty}>
+            Beleza
+          </div>
+        </div>
+        <div className="grid">
+          <div onClick={toAnimals}>
+            Pets
+          </div>
+          <div onClick={toHealth}>
+            Saúde
+          </div>
+        </div>
+        <hr />
         <h2>Explore diversos produtos...</h2>
         <div className="products-inline">
           <img src={data[3].image} alt={data[3].name} />
